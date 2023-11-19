09422d70476be95e6a0dde21aad6d3c9244a5bfe
shubham-tam <tamrakarshubham23@gmail.com>
2023-11-19 22:45:36 +0545

    product detail done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,5 +14,10 @@ export class RecipeListComponent {
       'This is simply a test',
       'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341'
     ),
+    new Recipe(
+      'A test recipe',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341'
+    ),
   ];
 }
